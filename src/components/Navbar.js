36205d645c3008b879e6ed9cde2b919b0aefca78
NIls ALberth <aliciallanos68@gmail.com>
2023-07-11 18:44:25 -0500

    css junto y muchas coasa ams

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import classNames from "classnames";
 import LineIcon from "react-lineicons";
 import { NavLink } from "react-bootstrap";
-import styles from "./App.module.css";
+import "./navbar.css";
 //import img1 from "./img/logo1.png";
 
 export const Navbar = () => {
@@ -12,33 +12,74 @@ export const Navbar = () => {
     setIsOpen(!isOpen);
   }
     return (
-        <>
-        <nav className={classNames(styles.navbar, "navbar navbar-dark navbar-expand-lg fixed-top bg-black-transparent shadow ")}>
-        <img src="/assets/Logo.png" width="50" height="60" alt="" />
-          
-          <ul className={styles.links__nav}>
-            <li className={styles.navbar__li}><a href="#" className={styles.navbar__a}>Home</a></li>
-            <li className={styles.navbar__li}><a href="#" className={styles.navbar__a}>Patio de comidas</a></li>
-            <li className={styles.navbar__li}><a href="#" className={styles.navbar__a}>Cine</a></li>
-            <li className={styles.navbar__li}><a href="#" className={styles.navbar__a}>Conciertos</a></li>
-            <li className={styles.navbar__li}><a href="#" className={styles.navbar__a}>Promociones</a></li>
-          </ul>
-          <a href="#" className={styles.action__btn}>Ingresar</a>
-          <div className={styles.toggle_btn} onClick={toggleMenu}>
-            <i className={isOpen ? 'lni lni-close' : 'lni lni-menu'}></i>
-          </div>
+      <>
+      <div className="row ">
+        <div className="container-fluid   overflow-hidden" >
+          <nav className={classNames("navbar", "navbar-expand-lg", "navbar-transparent", "fixed-top", "bg-black-transparent", "shadow")}>
+            <div className={classNames("container")}>
+              <a className={classNames("navbar-brand")} href="/home">
+                RecyTec
+              </a>
 
-          <div className={`${styles.dropdown_menu} ${isOpen ? styles.open : ''}`}>
-            <li className={styles.navbar__li}><a href="#" className={styles.navbar__a}>Home</a></li>
-            <li className={styles.navbar__li}><a href="#" className={styles.navbar__a}>Patio de comidas</a></li>
-            <li className={styles.navbar__li}><a href="#" className={styles.navbar__a}>Cine</a></li>
-            <li className={styles.navbar__li}><a href="#" className={styles.navbar__a}>Conciertos</a></li>
-            <li className={styles.navbar__li}><a href="#" className={styles.navbar__a}>Promociones</a></li>
-            <li><a href="#" className={styles.action__btn}>Ingresar</a></li>
-          </div>
+              <a href="ticket.html" className={classNames("btn", "custom-btn", "d-lg-none", "ms-auto", "me-4")}>
+                Buy Ticket
+              </a>
 
-        </nav>
-        </>
+              <button
+                className={classNames("navbar-toggler")}
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className={classNames("navbar-toggler-icon")}></span>
+              </button>
+
+              <div className={classNames("collapse", "navbar-collapse")} id="navbarNav">
+                <ul className={classNames("navbar-nav", "align-items-lg-center", "ms-auto", "me-lg-5")}>
+                  <li className={classNames("nav-item")}>
+                    <a className={classNames("nav-link", "click-scroll")} href="#section_1">
+                      Inicio
+                    </a>
+                  </li>
+
+                  <li className={classNames("nav-item")}>
+                    <a className={classNames("nav-link", "click-scroll")} href="#section_2">
+                      Acerca de
+                    </a>
+                  </li>
+
+                  <li className={classNames("nav-item")}>
+                    <a className={classNames("nav-link", "click-scroll")} href="#section_3">
+                      ¿Que puedo reciclar?
+                    </a>
+                  </li>
+
+                  <li className={classNames("nav-item")}>
+                    <a className={classNames("nav-link", "click-scroll")} href="#section_4">
+                      Ideas
+                    </a>
+                  </li>
+
+                  <li className={classNames("nav-item")}>
+                    <a className={classNames("nav-link", "click-scroll")} href="#section_6">
+                      ¿Quieres ser parte?
+                    </a>
+                  </li>
+                </ul>
+
+                <a href="ticket.html" className={classNames("btn", "custom-btn", "d-lg-block", "d-none")}>
+                  Registro
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+
+    </>
     );
 };
 export default Navbar;
