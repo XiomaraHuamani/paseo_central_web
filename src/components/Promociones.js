69af21fc4744d2f189da3bc75608d3d7aa956c55
NIls ALberth <aliciallanos68@gmail.com>
2023-07-15 19:45:51 -0500

    avances 3

--- src/components/Promociones.js
+++ src/components/Promociones.js
@@ -6,27 +6,47 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import React from "react";
 import classNames from "classnames";
-import img1 from "./img/promo1.jpg"
-import img2 from "./img/promo2.jpg"
-import img3 from "./img/promo3.jpg"
-import img4 from "./img/promo4.jpg"
+import img1 from "./img/promo1.jpg";
+import img2 from "./img/promo2.jpg";
+import img3 from "./img/promo3.jpg";
+import img4 from "./img/promo4.jpg";
 import styles from "./App.module.css";
 import "./navbar.css";
 export const Promociones = () => {
   return (
     <>
-      <div
-        className={classNames(
-          styles.promos,
-          "container-fluid"
-        )}
-      >
+      <div className={classNames(styles.promos, "container-fluid")}>
         <h1>Promociones del mes</h1>
         <Swiper
           slidesPerView={3}
           spaceBetween={30}
           className={styles.myswiper_promo}
+          breakpoints={{
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+              slidesPerGroup: 1,
+            },
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 1,
+              spaceBetween: 30,
+              slidesPerGroup: 1,
+            },
+            600: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+              slidesPerGroup: 1,
+            },
 
+            // when window width is >= 640px
+            640: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+              slidesPerGroup: 3,
+            },
+          }}
         >
           <SwiperSlide>
             <div className={classNames("card", styles.promo_card)}>
@@ -38,16 +58,15 @@ export const Promociones = () => {
                   <div className={classNames(styles.promo_con)}>
                     <h6>Desceunto en cine solo en paso central </h6>
                     <br />
-                    <p>d  isfruta de tu cine al 50% solo en paseo central solo con pelicula en la lista
+                    <p>
+                      d isfruta de tu cine al 50% solo en paseo central solo con
+                      pelicula en la lista
                     </p>
-                    
-                    <a href="#" className="button ">
 
+                    <a href="#" className="button ">
                       Mas ifno
                     </a>
-
                   </div>
-
                 </center>
               </div>
             </div>
@@ -62,15 +81,15 @@ export const Promociones = () => {
                   <div className={classNames(styles.promo_con)}>
                     <h6>Desceunto en cine solo en paso central </h6>
                     <br />
-                    <p>disfruta de tu cine al 50% solo en paseo central solo con pelicula en la lista
+                    <p>
+                      disfruta de tu cine al 50% solo en paseo central solo con
+                      pelicula en la lista
                     </p>
 
                     <a href="#" className="button">
                       Mas ifno
                     </a>
-
                   </div>
-
                 </center>
               </div>
             </div>
@@ -85,15 +104,15 @@ export const Promociones = () => {
                   <div className={classNames(styles.promo_con)}>
                     <h6>Desceunto en cine solo en paso central </h6>
                     <br />
-                    <p>disfruta de tu cine al 50% solo en paseo central solo con pelicula en la lista
+                    <p>
+                      disfruta de tu cine al 50% solo en paseo central solo con
+                      pelicula en la lista
                     </p>
 
                     <a href="#" className="button">
                       Mas ifno
                     </a>
-
                   </div>
-
                 </center>
               </div>
             </div>
@@ -108,22 +127,21 @@ export const Promociones = () => {
                   <div className={classNames(styles.promo_con)}>
                     <h6>Desceunto en cine solo en paso central </h6>
                     <br />
-                    <p>disfruta de tu cine al 50% solo en paseo central solo con pelicula en la lista
+                    <p>
+                      disfruta de tu cine al 50% solo en paseo central solo con
+                      pelicula en la lista
                     </p>
 
                     <a href="#" className="button">
                       Mas ifno
                     </a>
-
                   </div>
-
                 </center>
               </div>
             </div>
           </SwiperSlide>
-
         </Swiper>
-      </div >
+      </div>
     </>
   );
 };
